eb6b945b2bec2acd7664f0813959ded20129c71b
Ubuntu <ubuntu@ip-172-31-88-36.ec2.internal>
2019-10-15 16:16:54 +0000

    Attempting to connect to MONGODB, but error No module named flask_pymongo

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = 'mongodb+srv://RobSimons1:<password>@myfirstcluster-yyi3w.mongodb.net/task_manager?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
 if __name__ == '__main__':
