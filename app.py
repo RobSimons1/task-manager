1af565f66d8936573fab31acd66c872606b97d8b
Ubuntu <ubuntu@ip-172-31-88-36.ec2.internal>
2019-10-16 09:54:06 +0000

    Attempting to connect MONGODB with App

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://RobSimons1:<password>@myfirstcluster-yyi3w.mongodb.net/task_manager?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = 'mongodb+srv://RobSimons1:Ripped14@myfirstcluster-yyi3w.mongodb.net/task_manager?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
-
-@app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    _tasks = mongo.db.tasks.find()
+    task_list = [task for task in _tasks]
+    return render_template("tasks.html", task = task_list)
+
+
+@app.route('/add_task')
+def add_task():
+    _categories = mongo.db.categories.find()
+    category_list = [category for category in _categories]
+    return render_template('addtask.html', categories = category_list)
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
+        port=int(os.environ.get('PORT')),
+        debug=True)
