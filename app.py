d9f6888dd93055a4abe7f26ec30883fc2658cc5c
Ubuntu <ubuntu@ip-172-31-88-36.ec2.internal>
2019-10-18 13:43:15 +0000

    added Edit Task to Task Manager. Need to input MONGODB password

--- app.py
+++ app.py
@@ -3,29 +3,56 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://RobSimons1:Ripped14@myfirstcluster-yyi3w.mongodb.net/task_manager?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = 'mongodb+srv://RobSimons1:<password>@myfirstcluster-yyi3w.mongodb.net/task_manager?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
-
+@app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    _tasks = mongo.db.tasks.find()
-    task_list = [task for task in _tasks]
-    return render_template("tasks.html", task = task_list)
+    return render_template("tasks.html", 
+                           tasks=mongo.db.tasks.find())
 
 
-@app.route('/')
 @app.route('/add_task')
 def add_task():
-    _categories = mongo.db.categories.find()
-    category_list = [category for category in _categories]
-    return render_template('addtask.html', categories = category_list)
+    return render_template('addtask.html',
+                           categories=mongo.db.categories.find())
+
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks =  mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
+
+
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    the_task =  mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('edittask.html', task=the_task,
+                           categories=all_categories)
+
+
+@app.route('/update_task/<task_id>', methods=["POST"])
+def update_task(task_id):
+    tasks = mongo.db.tasks
+    tasks.update( {'_id': ObjectId(task_id)},
+    {
+        'task_name':request.form.get('task_name'),
+        'category_name':request.form.get('category_name'),
+        'task_description': request.form.get('task_description'),
+        'due_date': request.form.get('due_date'),
+        'is_urgent':request.form.get('is_urgent')
+    })
+    return redirect(url_for('get_tasks'))
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
-        debug=True)
+            port=int(os.environ.get('PORT')),
+            debug=True)
